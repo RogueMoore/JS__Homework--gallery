7162ac8ea4752bfdc99023ac791ee6d8554e7c50
Vitaly Grodetsky <rogue.moore.private@gmail.com>
2024-12-04 21:02:13 +0200

    added styles and pixabay api

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,8 @@
 // insert your code here
+
+const API_KEY = '47398342-c4f399e57f5c71f1dbfff10b5';
+const PIXABAY_API = 'https://pixabay.com/api/';
+
+const gallery = document.querySelector('.gallery');
+
+
