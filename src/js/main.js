4236b4e0cc94007aab2472693ac87f01a6e2a970
Vitaly Grodetsky <rogue.moore.private@gmail.com>
2024-12-09 13:26:53 +0200

    added gallery-cards, waitNotify and some styles for cards

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,61 @@
 // insert your code here
 
 const API_KEY = '47398342-c4f399e57f5c71f1dbfff10b5';
-const PIXABAY_API = 'https://pixabay.com/api/';
+const BASE_URL = 'https://pixabay.com/api/';
 
 const gallery = document.querySelector('.gallery');
+const loadMoreBtn = document.querySelector('.load-more');
+console.log(gallery);
 
+async function loadData() {
+  try {
+    const waitNotify = document.createElement('p');
+    waitNotify.textContent = 'Loading images, please wait...';
+    waitNotify.classList.add('waitNotify')
+    loadMoreBtn.insertAdjacentElement('beforebegin', waitNotify);
+
+    const response = await fetch(`${BASE_URL}?key=${API_KEY}`);
+
+    if (response.ok) {
+      waitNotify.remove();
+
+      const data = await response.json();
+      console.log(data);
+
+      gallery.innerHTML = renderCards(data.hits);
+    }
+  } catch {}
+}
+
+loadData();
+
+function createCard(card) {
+  return `
+    <div class="photo-card">
+      <img src="${card.previewURL}" alt="${card.tags}" loading="lazy" />
+      <div class="info">
+        <p class="info-item">
+          <b>Likes</b>
+          ${card.likes}
+        </p>
+        <p class="info-item">
+          <b>Views</b>
+          ${card.views}
+        </p>
+        <p class="info-item">
+          <b>Comments</b>
+          ${card.comments}
+        </p>
+        <p class="info-item">
+          <b>Downloads</b>
+          ${card.downloads}
+        </p>
+        </div>
+      </div>
+   `;
+}
+
+function renderCards(cards) {
+  return cards.map((c) => createCard(c)).join('');
+}
 
